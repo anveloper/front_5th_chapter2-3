6b7ad8e542291da0ca2e974cfb100abf7bbb868b
anveloper <hitedin@gmail.com>
2025-04-26 23:46:06 +0900

    refactor: app 폴더 구성, App.tsx 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import PostsManagerPage from "./pages/PostsManagerPage.tsx";
-import Footer from "./widgets/ui/Footer.tsx";
-import Header from "./widgets/ui/Header.tsx";
+import PostsManagerPage from "../pages/PostsManagerPage.tsx";
+import Footer from "../widgets/ui/Footer.tsx";
+import Header from "../widgets/ui/Header.tsx";
+import Providers from "./providers";
+import "./styles/global.css";
 
 const App = () => {
   return (
-    <Router>
+    <Providers>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">
@@ -13,7 +14,7 @@ const App = () => {
         </main>
         <Footer />
       </div>
-    </Router>
+    </Providers>
   );
 };
 
---
+++ src/app/providers/index.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <Router>{children}</Router>;
+};
+
+export default Providers;
