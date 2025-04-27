7cb8ea8754d4424f6f85138014f3372ed31f08fa
anveloper <hitedin@gmail.com>
2025-04-27 18:52:08 +0900

    fix: 라우트 만 프로바이더 분리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import PostsManagerPage from "../pages/PostsManagerPage.tsx";
 import Footer from "../widgets/ui/Footer.tsx";
 import Header from "../widgets/ui/Header.tsx";
@@ -6,15 +7,17 @@ import "./styles/global.css";
 
 const App = () => {
   return (
-    <Providers>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Providers>
+    <Router>
+      <Providers>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Providers>
+    </Router>
   );
 };
 
