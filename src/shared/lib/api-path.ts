0cd006ca39c7bdb25905dc9ce412b09b4968e9ab
anveloper <hitedin@gmail.com>
2025-04-29 23:36:26 +0900

    chore: 배포 환경에서도 더미 API 요청

--- src/shared/lib/api-path.ts
+++ src/shared/lib/api-path.ts
@@ -3,7 +3,7 @@ import { Post } from "@/entities/post/models";
 import { User } from "@/entities/user/models/user.types";
 
 // SEGMENTS
-const _API = "api";
+const _API = import.meta.env.DEV ? "/api" : "https://dummyjson.com";
 const _ADD = "add";
 const _POSTS = "posts";
 const _COMMENTS = "comments";
@@ -16,19 +16,19 @@ const _POST = "post";
 // URL
 export const API_URL = {
   // posts
-  POSTS: (limit: number, skip: number) => `/${_API}/${_POSTS}?limit=${limit}&skip=${skip}` as const,
-  POSTS_ID: (id: Post["id"]) => `/${_API}/${_POSTS}/${id}` as const,
-  POSTS_ADD: `/${_API}/${_POSTS}/${_ADD}`,
-  POSTS_TAG: (tag: Post["tags"][number]) => `/${_API}/${_POSTS}/${_TAG}/${tag}` as const,
-  POSTS_TAGS: `/${_API}/${_POSTS}/${_TAGS}`,
-  POSTS_SEARCH: (query: string) => `/${_API}/${_POSTS}/${_SEARCH}?q=${query}` as const,
+  POSTS: (limit: number, skip: number) => `${_API}/${_POSTS}?limit=${limit}&skip=${skip}` as const,
+  POSTS_ID: (id: Post["id"]) => `${_API}/${_POSTS}/${id}` as const,
+  POSTS_ADD: `${_API}/${_POSTS}/${_ADD}`,
+  POSTS_TAG: (tag: Post["tags"][number]) => `${_API}/${_POSTS}/${_TAG}/${tag}` as const,
+  POSTS_TAGS: `${_API}/${_POSTS}/${_TAGS}`,
+  POSTS_SEARCH: (query: string) => `${_API}/${_POSTS}/${_SEARCH}?q=${query}` as const,
 
   // comments
-  COMMENTS_ID: (id: Comment["id"]) => `/${_API}/${_COMMENTS}/${id}` as const,
-  COMMENTS_ADD: `/${_API}/${_COMMENTS}/${_ADD}`,
-  COMMENTS_POST_ID: (id: Post["id"]) => `/${_API}/${_COMMENTS}/${_POST}/${id}` as const,
+  COMMENTS_ID: (id: Comment["id"]) => `${_API}/${_COMMENTS}/${id}` as const,
+  COMMENTS_ADD: `${_API}/${_COMMENTS}/${_ADD}`,
+  COMMENTS_POST_ID: (id: Post["id"]) => `${_API}/${_COMMENTS}/${_POST}/${id}` as const,
 
   // users
-  USERS: `/${_API}/${_USERS}?limit=0&select=username,image`,
-  USERS_ID: (id: User["id"]) => `/${_API}/${_USERS}/${id}` as const,
+  USERS: `${_API}/${_USERS}?limit=0&select=username,image`,
+  USERS_ID: (id: User["id"]) => `${_API}/${_USERS}/${id}` as const,
 } as const;
